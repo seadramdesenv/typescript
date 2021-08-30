569e6e232c19e7d2ea766c54ad0f5f8c0a0f599d
Allan Campos <seadramdesenv@gmail.com>
2021-08-30 14:38:29 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,31 @@
+let message: string = "Help me"
+console.log(message);
+
+let episode: number = 4
+console.log("Episodio é " + 4)
+episode = episode + 1
+console.log("proximo Episodio é " + episode)
+
+let favorite: string
+favorite = 'BB-8'
+console.log("Meu droide favorito é " + favorite)
+
+
+let MF = function(parsecs: number): boolean{
+    return parsecs < 12
+}
+let distance = 11
+console.log(`Is ${distance} passa a MF em segundos? ${MF(distance) ? 'SIM': 'NÃO'} ` )
+
+
+let call = (name: string) => console.log(`Do you copy, ${name}`)
+call('R2')
+
+
+
+function inc (speed: number, inc: number = 1): number{
+    return speed + inc
+}
+
+console.log(`inc (5,1) = ${inc(5,1)}`)
+console.log(`inc (5) = ${inc(5)}`)
---
+++ dist/app.js
@@ -0,0 +1,22 @@
+var message = "Help me";
+console.log(message);
+var episode = 4;
+console.log("Episodio é " + 4);
+episode = episode + 1;
+console.log("proximo Episodio é " + episode);
+var favorite;
+favorite = 'BB-8';
+console.log("Meu droide favorito é " + favorite);
+var MF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " passa a MF em segundos? " + (MF(distance) ? 'SIM' : 'NÃO') + " ");
+var call = function (name) { return console.log("Do you copy, " + name); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5,1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
